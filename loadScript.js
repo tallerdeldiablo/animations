07d1975f60b85466a612c203120779b6d24c7973
Ricoding <rikardoan@gmail.com>
2024-12-20 23:24:01 -0500

    cambios test

--- loadScript.js
+++ loadScript.js
@@ -1,32 +1,32 @@
 const displayRepoContents = contents => {
-    repoContainerEl.innerHTML = '';
-    contents.forEach(item => {
-      if (item.type === 'dir') {
-        const button = document.createElement('button');
-        button.textContent = `${item.name} Animation`;
-        button.classList.add('btn');
-        button.onclick = () => {
-          // Actualizar el iframe para mostrar la animación
-          const animationContainerEl = document.querySelector('#animation-container');
-          animationContainerEl.src = `https://tallerdeldiablo.github.io/animations/${item.name}/`;
-  
-          // Cargar el contenido del archivo txt en texto plano
-          const scriptBox = document.querySelector('#script-box');
-          const scriptFile = `${item.name}/script.txt`;
-          fetch(scriptFile)
-            .then(response => {
-              if (!response.ok) throw new Error('File not found');
-              return response.text();
-            })
-            .then(data => {
-              scriptBox.textContent = data; // Asegúrate de usar textContent para texto plano
-            })
-            .catch(() => {
-              scriptBox.textContent = 'Script not available.';
-            });
-        };
-        repoContainerEl.appendChild(button);
-      }
-    });
-  };
-  
+  repoContainerEl.innerHTML = '';
+
+  // Saltar el primer elemento
+  contents.slice(1).forEach(item => {
+    if (item.type === 'dir') {
+      const button = document.createElement('button');
+      button.textContent = `${item.name} Animation`;
+      button.classList.add('btn');
+      button.onclick = () => {
+        const animationContainerEl = document.querySelector('#animation-container');
+        animationContainerEl.src = `https://tallerdeldiablo.github.io/animations/${item.name}/`;
+
+        const scriptBox = document.querySelector('#script-box');
+        const scriptFile = `${item.name}/script.txt`;
+        fetch(scriptFile)
+          .then(response => {
+            if (!response.ok) throw new Error('File not found');
+            return response.text();
+          })
+          .then(data => {
+            // Reemplaza saltos de línea por <br> para respetar el formato
+            scriptBox.innerHTML = data.replace(/\n/g, '<br>');
+          })
+          .catch(() => {
+            scriptBox.textContent = 'Script not available.';
+          });
+      };
+      repoContainerEl.appendChild(button);
+    }
+  });
+};
